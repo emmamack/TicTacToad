34e2ae69cebabff980774a8b971a1a4372a62897
emmamack <emack@multisensorscientific.com>
2022-02-24 19:43:04 -0500

    Add beginning of 20q two-way, still need to check for correct answer

--- socket_test.c
+++ socket_test.c
@@ -28,12 +28,10 @@ int read_in(int socket, char *buf, int len) {
   char *s = buf;
   int slen = len;
   int c = recv(socket, s, slen, 0);
-  printf("recv: %d", c);
   while ((c > 0) && (s[c-1] != '\n')) {
     s += c; slen -= c;
     c = recv(socket, s, slen, 0);
   }
-  printf("Got out of the while loop");
   if (c < 0)
     return c;
   else if (c == 0)
@@ -71,16 +69,51 @@ int main() {
     if (connect_d == -1)
       error("Can't open secondary socket");
     printf("connect_d: %d\n", connect_d);
-    printf("sending now\n");
 
-    char *msg = "Hello world!\n";
+    char *msg = "Welcome to 20 questions!\n";
     if (send(connect_d, msg, strlen(msg), 0) == -1)
       error("Can't send");
 
+    printf("Welcome to 20 questions!\n");
+    char answer_array[80];
+    char *answer_str = answer_array;
+    puts("Player 1: What are you thinking of? ");
+    scanf("%s", answer_str);
+
     char buf[255];
-    printf("Starting read\n");
-    read_in(connect_d, buf, sizeof(buf));
-    printf("Response: %s\n", buf);
+
+    for (int i = 1; i < 21; i++) {
+        // Player 1 asks a question, which is saved
+        char *msg2 = "Player 2: Ask a yes or no question: \n";
+        send(connect_d, msg2, strlen(msg2), 0);
+        char guess_array[255];
+        read_in(connect_d, guess_array, sizeof(buf));
+        char* guess = guess_array;
+        printf("%s\n", guess_array);
+
+        // check for correct guess
+        printf("guess: %s\n", guess);
+        printf("answer_str: %s\n", answer_str);
+        printf("len guess: %d\n", strlen(guess));
+        printf("len answer_str: %d\n", strlen(answer_str));
+        int res = strcmp(answer_str, guess);
+        printf("res: %d\n", res);
+        if (!res) {
+          printf("All done!!!!");
+        }
+
+        // Player 2 answers Y/N
+        puts("Player 1: Enter Y or N, or C if it is the correct answer: ");
+        scanf("%s", buf);
+        char *response = strcat(buf, "\n");
+        send(connect_d, response, strlen(response), 0);
+
+        // Check for correct guess
+        // if (guess_str[0] == 'C') {
+        //     printf("Correct - Player 1 was thinking of \"%s\"! Guessed in %i questions.\n", answer_str, i);
+        //     break;
+        // }
+    }
 
     close(connect_d);
 
