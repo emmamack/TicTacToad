2b32c5632e1bf0f7f7e179c9641f3c17875d6ec9
emmamack <emack@multisensorscientific.com>
2022-03-06 16:02:25 -0500

    Fix 20q strcmp error, clean up 20q gameplay

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int read_in(int socket, char *buf, int len) {
 int main() {
   int listener_d = socket(PF_INET, SOCK_STREAM, 0);
   if (listener_d == -1) error("Can't open socket");
-  printf("Listener_d: %d\n", listener_d);
+  // printf("Listener_d: %d\n", listener_d);
 
   struct sockaddr_in name;
   name.sin_family = PF_INET;
@@ -56,12 +56,11 @@ int main() {
   int c = bind(listener_d, (struct sockaddr *) &name, sizeof(name));
   if (c==-1)
     error("Can't bind to socket");
-  printf("c: %d\n", c);
 
   if (listen(listener_d, 10) == -1)
     error("Can't listen");
 
-  puts("Waiting...");
+  puts("Waiting for a player to join...");
 
   while(1) {
 
@@ -70,61 +69,90 @@ int main() {
     int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
     if (connect_d == -1)
       error("Can't open secondary socket");
-    printf("connect_d: %d\n", connect_d);
+    // printf("connect_d: %d\n", connect_d);
 
     // Play games!
     play_20q(connect_d);
+    c = close(connect_d);
+    // printf("c: %d\n", c);
+    if (c==-1) {
+      error("Could not close socket");
+    }
 
-    close(connect_d);
+    printf("Waiting for next player... press ctrl+C to quit\n");
+
   }
 
   return 0;
 }
 
+void print_ints(char* s) {
+  if (*s == '\0') {
+    printf("slash0\n");
+    return;
+  }
+  printf("%d ", *s);
+  print_ints(s+1);
+}
+
+/* Alternate form of strcmp that ignores carriage returns towards inequality
+since carriage returns are appended on local strings but not ones sent through
+telnet.
+Returns 1 for "same" and 0 for "different"
+*/
+int strcmp_CRignore(char* s1, char* s2) {
+  while (1) {
+    if ((*s1=='\r' && *s2=='\0') ||
+        (*s1=='\0' && *s2=='\r') ||
+        (*s1=='\r' && *s2=='\r') ||
+        (*s1=='\0' && *s2=='\0')) {
+           return 1;
+    }
+
+    if (*s1 != *s2) {
+      return 0;
+    }
+
+    s1++; s2++;
+  }
+}
 
 void play_20q(int connect_d) {
-    char *msg = "Welcome to 20 questions!\n";
+
+    // send intro messages
+    char *msg = "Welcome to 20 questions, player 2! Player 1 is thinking of a secret word...\n";
     if (send(connect_d, msg, strlen(msg), 0) == -1)
       error("Can't send");
-
-    printf("Welcome to 20 questions!\n");
     char answer_array[80];
     char *answer_str = answer_array;
-    puts("Player 1: What are you thinking of? ");
+    puts("Welcome to 20 questions! Player 1, what word are you thinking of? ");
     scanf("%s", answer_str);
 
     char buf[255];
 
     for (int i = 1; i < 21; i++) {
-        // Player 1 asks a question, which is saved
-        char *msg2 = "Player 2: Ask a yes or no question: \n";
+        char *msg2 = "Player 2: Ask a yes or no question, or enter a guess: \n";
         send(connect_d, msg2, strlen(msg2), 0);
+
+        // read player 2 guess
         char guess_array[80];
-        read_in(connect_d, guess_array, sizeof(buf));
+        read_in(connect_d, guess_array, sizeof(guess_array));
         char* guess = guess_array;
         printf("%s\n", guess_array);
 
         // check for correct guess
-        printf("guess: %s\n", guess);
-        printf("answer_str: %s\n", answer_str);
-        printf("len guess: %zu\n", strlen(guess));
-        printf("len answer_str: %zu\n", strlen(answer_str));
-        int res = strcmp(answer_str, guess);
-        printf("res: %d\n", res);
-        if (!res) {
-          printf("All done!!!!");
+        int res = strcmp_CRignore(answer_str, guess);
+        if (res) {
+          printf("Player 2 guessed the answer: \"%s\"!  Guessed in %i questions.\n", answer_str, i);
+          char *end_msg = "You guessed it! Goodbye.\n";
+          send(connect_d, end_msg, strlen(msg2), 0);
+          break;
         }
 
-        // Player 2 answers Y/N
-        puts("Player 1: Enter Y or N, or C if it is the correct answer: ");
+        // Player 1 answers Y/N
+        printf("Player 1: answer yes or no: ");
         scanf("%s", buf);
         char *response = strcat(buf, "\n");
         send(connect_d, response, strlen(response), 0);
-
-        // Check for correct guess
-        if (response[0] == 'C') {
-            printf("Correct - Player 1 was thinking of \"%s\"! Guessed in %i questions.\n", answer_str, i);
-            break;
-        }
     }
 }
